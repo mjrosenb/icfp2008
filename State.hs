cd7b2db602bbf80b3784682648820a29c0a1b505
Marty Rosenberg <martin.rosenberg@schrodinger.com>
2023-01-24 12:37:41 -0500

    Add State.hs

---
+++ State.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE TemplateHaskell #-}
+module State where
+import qualified Message as Msg
+import qualified Data.Set as S
+import Control.Lens
+data SpiralingSt = SpiralingSt { _straightCount :: Int
+                               , _maxStraight :: Int
+                               } deriving (Show, Eq)
+data EndInSightSt = EndInSightSt { _endX :: Double
+                                 , _endY :: Double
+                                 } deriving (Show, Eq)
+data SMState = Spiraling  SpiralingSt
+             | EndInSight EndInSightSt deriving (Show, Eq)
+
+
+
+data DriveState = DriveState { _objects :: S.Set Msg.Object
+                             , _smState :: SMState
+                             } deriving (Show, Eq)
+makeLenses ''DriveState
+makeLenses ''SpiralingSt
+makeLenses ''EndInSightSt
+makePrisms ''SMState
+initState = DriveState { _objects = S.empty
+                       , _smState = Spiraling SpiralingSt { _straightCount = 0
+                                                          , _maxStraight = 10
+                                                          }
+                       }
