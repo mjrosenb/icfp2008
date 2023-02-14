78f3ac3b5750f35e33bd47c8ef9b19e362fb34df
Marty Rosenberg <martin.rosenberg@schrodinger.com>
2023-02-14 13:32:37 -0500

    Misc improvements to the dstar state:
    * Split DStar cells into drivable and obstacles
    * make an obstacle border around the field

--- State.hs
+++ State.hs
@@ -14,21 +14,32 @@ data DStarStatus =
   | LOWER
   deriving (Show, Eq)
 
-data DStarCell = DSC { _status :: DStarStatus
-                     , _nextCellInPath :: Maybe GridPoint
-                     } deriving (Show, Eq)
+data DStarCell = Drivable { _status :: DStarStatus
+                          , _nextCellInPath :: Maybe GridPoint
+                          , _currentCost :: Int
+                          }
+               | Obstacle
+  deriving (Show, Eq)               
 
 type DStarState = A.Array GridPoint DStarCell
 data DriveState = DriveState { _objects :: S.Set Msg.Object
                              , _dStarState :: DStarState
                              } deriving (Show, Eq)
 makeLenses ''DriveState
+makeLenses ''DStarCell
+makePrisms ''DStarCell
+
 initState P {_x=x, _y=y} =
   let hx = ceiling (x / 2)
       hy = ceiling (y / 2)
+      bx = hx+1
+      by = hy+1
+      meat = [(P x y, Drivable NEW Nothing (x*y*2)) | x <- [-hx..hx], y <- [-hy, hy]]
+      border = [(P x y, Obstacle) | (rx, ry) <- [([-bx..bx],[-by,by]), ([-bx,bx],[-by..by])]
+                                  , x <- rx, y <- ry]
   in 
     DriveState { _objects = S.empty
-               , _dStarState = A.listArray
-                               (P (negate hx) (negate hy), P hx hy)
-                               (repeat DSC {_status = NEW, _nextCellInPath = Nothing })
+               , _dStarState = A.array
+                               (P (negate bx) (negate by), P bx by)
+                               (meat ++ border)
                }
